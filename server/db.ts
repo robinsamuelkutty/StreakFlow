ee694035202bb885dd359a9e923c85ae0a12566e
Robin Samuelkutty <robinsam754@gmail.com>
2025-11-27 17:33:09 +0530

    partially completed

--- server/db.ts
+++ server/db.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
@@ -5,10 +6,12 @@ import * as schema from "@shared/schema";
 
 neonConfig.webSocketConstructor = ws;
 
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 if (!process.env.DATABASE_URL) {
   throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+    "DATABASE_URL must be set. Did you forget to provision a database?"
+  )
 }
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
@@ -89,7 +91,7 @@ app.use((req, res, next) => {
     {
       port,
       host: "0.0.0.0",
-      reusePort: true,
+      
     },
     () => {
       log(`serving on port ${port}`);
